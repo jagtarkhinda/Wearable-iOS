025b6428e4dabbe528dc0f12cde346eecef6f3e7
MacStudent <macstudent@130-IMAC-24.local>
2019-06-18 10:47:06 -0400

    first commit

---
+++ Wearable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wearable
+//
+//  Created by MacStudent on 2019-06-18.
+//  Copyright © 2019 Jg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
